d8dae171a051ce0bf75423d5c2b9713f787f1bcc
Nastya Ladynskaya <feverrr27@gmail.com>
2022-12-23 16:37:03 +0200

    papaparse

--- src/tasks/LoadCountriesTask.js
+++ src/tasks/LoadCountriesTask.js
@@ -1,13 +1,21 @@
+import papa from "papaparse";
 import { features } from "../data/countries.json";
 
 class LoadCountriesTask {
   covidUrl =
     "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_country.csv";
 
+  setState = null;
+
   load = (setState) => {
-    setState(features);
-    console.log(features);
+    this.setState = setState;
+
+    papa.parse(this.covidUrl, {
+      download: true,
+      header: true,
+      complete: (result) => this.#processCovidData(result.data),
+    });
   };
-};
+}
 
 export default LoadCountriesTask;
