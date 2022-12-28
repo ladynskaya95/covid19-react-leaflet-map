1081974cf1a46ff4fcc543043704860f593b249e
Nastya Ladynskaya <feverrr27@gmail.com>
2022-12-28 13:55:42 +0200

    covid data

--- src/tasks/LoadCountriesTask.js
+++ src/tasks/LoadCountriesTask.js
@@ -16,6 +16,39 @@ class LoadCountriesTask {
       complete: (result) => this.#processCovidData(result.data),
     });
   };
+  #processCovidData = (covidCountries) => {
+    for (let i = 0; i < features.length; i++) {
+      const country = features[i];
+      //console.log(country);
+      const covidCountry = covidCountries.find(
+        (covidCountry) => country.properties.ISO_A3 === covidCountry.ISO3
+      );
+
+      country.properties.confirmed = 0;
+      country.properties.confirmedText = 0;
+
+      if (covidCountry != null) {
+        let confirmed = Number(covidCountry.Confirmed);
+        country.properties.confirmed = confirmed;
+        country.properties.confirmedText =
+          this.#formatNumberWithCommas(confirmed);
+      }
+      // this.#setCountryColor(country);
+    }
+
+    this.setState(features);
+  };
+  // #setCountryColor = (country) => {
+  //   const legendItem = legendItems.find((item) =>
+  //     item.isFor(country.properties.confirmed)
+  //   );
+
+  //   if (legendItem != null) country.properties.color = legendItem.color;
+  // };
+
+  #formatNumberWithCommas = (number) => {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
 }
 
 export default LoadCountriesTask;
